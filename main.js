b1eb69c95746def445f02e5887e17c14dcb2b8fb
Ritu0097 <rituchaudhary0097@gmail.com>
2024-06-05 13:25:40 +0530

    server

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  // TODO: Send the ""Hello, world!"" response
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, world!');
 });
 
 server.listen(port, () => {
-  // TODO: Log a message to the console when the server starts listening on the port
+  console.log(`Server is running on http://localhost:${port}`);
 });
 
 // DO NOT EDIT BELOW THIS LINE
